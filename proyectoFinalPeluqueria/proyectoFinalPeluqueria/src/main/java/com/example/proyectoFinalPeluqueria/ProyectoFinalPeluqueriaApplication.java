0ee832f6f906e12d43f7d9351078b88090853179
isabeCastillo <139247973+isabeCastillo@users.noreply.github.com>
2024-11-15 13:14:34 -0600

    inicio del proyecto

---
+++ proyectoFinalPeluqueria/proyectoFinalPeluqueria/src/main/java/com/example/proyectoFinalPeluqueria/ProyectoFinalPeluqueriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoFinalPeluqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPeluqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPeluqueriaApplication.class, args);
+	}
+
+}
